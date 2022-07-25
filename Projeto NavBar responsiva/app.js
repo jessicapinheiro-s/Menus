20c26f511e3331a75ffa2664a65727baf41c1722
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-07-25 13:30:55 -0300

    criado uma nova function para remover

--- Projeto NavBar responsiva/app.js
+++ Projeto NavBar responsiva/app.js
@@ -1,8 +1,19 @@
 document.getElementById('logoMenu').addEventListener('click', function openResponsiveMenu (){
     let menuLogoClose = document.querySelector('.close-logo-menu');
     let menuLogoOpen = document.querySelector('.logoMenu');
-
+    let navResponsive = document.querySelector('.navResponsive');
+    
+    navResponsive.classList.toggle('responsive');
     menuLogoClose.classList.toggle('responsive');
     menuLogoOpen.classList.toggle('remoResponsive');
     
+})
+
+document.getElementById('logoMenuClose').addEventListener('click', function openResponsiveMenu (){
+    let navResponsive = document.querySelector('.navResponsive');
+    let menuLogoClose = document.querySelector('.close-logo-menu');
+    let menuLogoOpen = document.querySelector('.logoMenu');
+    navResponsive.classList.remove('responsive');
+    menuLogoClose.classList.remove('responsive');
+    menuLogoOpen.classList.remove('remoResponsive');
 })
